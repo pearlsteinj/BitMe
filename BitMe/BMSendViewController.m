d2e19ce7d06deab04e8277ad84bf2a67e1162f43
Peter Bryan <peterbbryan@gmail.com>
2013-09-07 20:08:14 -0400

    Email Receipts
    Emai receipts are good

--- BitMe/BMSendViewController.m
+++ BitMe/BMSendViewController.m
@@ -56,13 +56,33 @@
             // Sorry for the scrappy work
             [[[lookup childByAppendingPath:user] childByAppendingPath:@"balance" ] setValue:[NSString stringWithFormat:@"%f", ([priorBalance floatValue] - [amount.text floatValue])]];
             [[[lookup childByAppendingPath:UID.text] childByAppendingPath:@"balance" ] setValue:[NSString stringWithFormat:@"%f", ([[[snapshot.value objectForKey:UID.text] objectForKey:@"balance"] floatValue] + [amount.text floatValue])]];
+            
+            
+            
+            Firebase* ref = [[Firebase alloc] initWithUrl:@"https://bitme.firebaseIO.com"];
+            FirebaseSimpleLogin* authClient = [[FirebaseSimpleLogin alloc] initWithRef:ref];
+            
+            //Check for Account
+            [authClient checkAuthStatusWithBlock:^(NSError* error, FAUser* user) {
+                if (error != nil) {                    
+                } else if (user == nil) {
+                }
+                else{
+                    NSString *url = [NSString stringWithFormat:@"http://www.sendgrid.com/api/mail.send.json?to=peterbbryan%%40gmail.com&toname=user&from=%@&fromname=BitMe&subject=You%%20received%%20BitCoins,%%20congratulations!&text=You%%20have%%20received%%20a%%20payment%%20of%%20%@&api_user=peterbbryan&api_key=walletBlock", user.email, amount.text];
+                    NSURLRequest *theRequest=[NSURLRequest requestWithURL:[NSURL URLWithString:url]
+                                                              cachePolicy:NSURLRequestUseProtocolCachePolicy
+                                                          timeoutInterval:60.0];
+                    // create the connection with the request
+                    // and start loading the data
+                    NSURLConnection *theConnection=[[NSURLConnection alloc] initWithRequest:theRequest delegate:self];
+                }
+            }];
         }
     }];
-    
 }
 
 -(void)sendPush{
-
+    
 }
 
 @end
